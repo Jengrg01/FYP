8576b44773d2c4c63aeee582d2f59165d31321ca
Jen-Grg <jengrg003@gmail.com>
2024-12-23 14:26:42 +0545

    Creation of artist card in home page

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from . models import Makeup #for entry to database
 # write functions for database, based on function or class based views(api creations get easier), we apure working on mvt pattern
 
 def home(request):
-    makeup = Makeup.objects.all()
+    makeup = Makeup.objects.all().order_by('-id')[:4]#creating of 4 artists to be displayed in the card, shows the latest added artist at first
     #the context is in dictionary form
     context ={
         "makeup": makeup
