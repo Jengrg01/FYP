67df0fa0f34b1b52146ef56a63d3c8796360ba5d
Muhammad Waqas <149311000+Muhammad-waqas1@users.noreply.github.com>
2025-01-12 12:43:32 +0500

    Update urls.py
    Added URLs for the Contact , Policy Terms and Privacy Policy page

--- app/urls.py
+++ app/urls.py
@@ -16,4 +16,7 @@ urlpatterns = [
     path("specialitylist/", views.specialitylist, name='specialitylist'),
     path("updatespeciality/<int:speciality_id>", views.updatespeciality, name='updatespeciality'),
     path("deletespeciality/<int:speciality_id>", views.deletespeciality, name='deletespeciality'),
+    path('contact/', views.contact, name='contact'),        
+    path('policy-terms/', views.policy_terms, name='policy_terms'), 
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
 ]
