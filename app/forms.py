b3a74c6084a32bf8d87db6d881b73af172e1b10a
Muhammad Waqas <149311000+Muhammad-waqas1@users.noreply.github.com>
2025-01-12 13:26:52 +0500

    Update forms.py
    Fixed some errors

--- app/forms.py
+++ app/forms.py
@@ -22,13 +22,7 @@ class SpecialityForm(ModelForm):
         fields = "__all__"
 
 
-class ContactForm(forms.ModelForm):
+class ContactForm(ModelForm):
     class Meta:
         model = ContactSubmission
         fields = ['name', 'email', 'subject', 'message']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}),
-            'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'}),
-            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Message'}),
-        }
